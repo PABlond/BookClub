f6de9a61b25ab80ae700844560f878d278b2e26b
Pierre-Alexis <pierre.alexis.blond@gmail.com>
2019-11-05 10:30:31 +0100

    add logout method

--- client/src/actions/auth/helpers/index.ts
+++ client/src/actions/auth/helpers/index.ts
@@ -6,6 +6,12 @@ export const setToken = (token: string) => {
   if (isWindow) window.localStorage.setItem(tokenName, token)
 }
 
-export const getToken = () => isWindow 
-  ? window.localStorage.getItem(tokenName) || ""
-  : ""
+export const getToken = () =>
+  isWindow ? window.localStorage.getItem(tokenName) || "" : ""
+
+export const logout = () => {
+  if (isWindow) {
+    window.localStorage.removeItem(tokenName)
+    window.location.href = "/login"
+  }
+}
