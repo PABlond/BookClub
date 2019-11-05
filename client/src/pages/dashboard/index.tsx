b1548cf87d69c73206b8387274ab2cdaa33d6964
Pierre-Alexis <pierre.alexis.blond@gmail.com>
2019-11-05 10:32:50 +0100

    update dashboard page

--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -1,16 +1,31 @@
-import React from "react"
+import React, { useState } from "react"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+import getBooks from "../../services/queries/searchBooks"
+import { IBook } from "../../interfaces/books.interface"
+
 import LoggedLayout from "../../components/Layouts/LoggedLayout"
 import AddBook from "../../components/AddBook"
 import Library from "../../components/Library"
-
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
+import Header from "../../components/Header/index.logged"
 
 export default ({}) => {
+  const [books, setBooks] = useState<IBook[]>([])
+  const [search, setSearch] = useState<string>("")
+
+  const searchBooks = async (e: any) => {
+    e.preventDefault()
+    setBooks(await getBooks(search))
+  }
+
+  const headerOpts = { search, setSearch, searchBooks }
+  const addBooksOpts = { books }
+
   return (
     <LoggedLayout>
+      <Header {...headerOpts} />
+      <AddBook {...addBooksOpts} />
       <Library />
-      <AddBook />
     </LoggedLayout>
   )
 }
