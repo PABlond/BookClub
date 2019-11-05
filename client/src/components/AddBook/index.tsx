d4fb8e344ec727ce82d0c01e1e2450a0b661c67a
Pierre-Alexis <pierre.alexis.blond@gmail.com>
2019-11-05 10:31:10 +0100

    update AddBook Component

--- client/src/components/AddBook/index.tsx
+++ client/src/components/AddBook/index.tsx
@@ -1,49 +1,41 @@
 import React, { useState, useEffect, Dispatch } from "react"
-import getBooks from "../../services/queries/searchBooks"
-import { IBook } from "../../interfaces/books.interface"
 import addBookToLib from "../../actions/library/addBookToLib"
+import { IBook } from "../../interfaces/books.interface"
 import { connect } from "react-redux"
 
-import { OverlayTrigger, Form, FormControl, Button } from "react-bootstrap"
+import { OverlayTrigger } from "react-bootstrap"
 import BookItems from "../BookItems"
-import BookDesc from "./BookDesc"
+import BookDesc from "../BookDesc"
+import { AddLibButton } from "../Styled"
+import { FaEdit } from "react-icons/fa"
 
-const AddBook = ({addBook}: any) => {
-  const [search, setSearch] = useState<string>("")
-  const [books, setBooks] = useState<IBook[]>([])
+const AddBook = ({ books, addBook }: { books: IBook[]; addBook: any }) => {
   const [selectI, setSelectI] = useState<number>(0)
   const [overI, setOverI] = useState<number>(0)
   const [show, setShow] = useState<Boolean>(true)
 
-  const searchBooks = async (e: any) => {
-    e.preventDefault()
-    setBooks(await getBooks(search))
+  const BookItemsOpts = {
+    books,
+    setSelectI,
+    overI,
+    setOverI,
+    title: "Add to your library",
   }
-  const BookItemsOpts = { books, setSelectI, overI, setOverI }
-  const BookDescOpts = { addBook, book: books[selectI], show, setShow }
-
-  const overlay = <BookDesc {...BookDescOpts} />
+  const BookDescOpts = {
+    book: books[selectI],
+    show,
+    setShow,
+  }
 
   const placement = "right-end"
 
   useEffect(() => {
     setShow(true)
   }, [selectI])
+
   return (
     <>
-      <Form inline onSubmit={searchBooks}>
-        <FormControl
-          type="text"
-          placeholder="Search"
-          className="mr-sm-2"
-          value={search}
-          onChange={(e: any) => setSearch(e.target.value)}
-        />
-        <Button type="submit" variant="outline-success">
-          Search
-        </Button>
-      </Form>
-      {books.length && (
+      {books.length ? (
         <>
           <OverlayTrigger
             trigger="click"
@@ -51,14 +43,18 @@ const AddBook = ({addBook}: any) => {
             rootCloseEvent={"mousedown"}
             rootClose={true}
             placement={placement}
-            overlay={overlay}
+            overlay={
+              <BookDesc {...BookDescOpts}>
+                <AddLibButton onClick={() => addBook(books[selectI].id)}>
+                  <FaEdit size="19px" /> Library
+                </AddLibButton>
+              </BookDesc>
+            }
           >
-            <div>
-              <BookItems {...BookItemsOpts} />
-            </div>
+            <BookItems {...BookItemsOpts} />
           </OverlayTrigger>
         </>
-      )}
+      ) : null}
     </>
   )
 }
@@ -69,7 +65,6 @@ const mapDispatchToProps = (dispatch: Dispatch<any>) => {
 }
 
 const mapStateToProps = (state: any) => {
-  // console.log(state)
   const { library } = state
   return { library }
 }
